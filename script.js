2954cbd54872c1d316f8bd8c7f12938f48c8eb92
Emily Dalton <emilydalton@dhcp-10-105-205-249.wireless.northwestern.private>
2018-11-16 10:57:17 -0600

    when I put the time cauclulations after the db push they started working

--- script.js
+++ script.js
@@ -6,56 +6,100 @@ var config = {
     projectId: "train-scheduler-9ae3c",
     storageBucket: "train-scheduler-9ae3c.appspot.com",
     messagingSenderId: "52421215999"
-  };
-  firebase.initializeApp(config);
+};
+firebase.initializeApp(config);
 
-  var dataRef = firebase.database();
+var dataRef = firebase.database();
 
-    // Initial variables
-    var train = "";
-    var destination = "";
-    var frequency =  0;
-    var arrival = "";
-    var firstTrain ="";
-    var table = "<tr><td id ='trainName' </td></tr> <td> <input type ='text' name ='lastname'> </td><td><input type ='text' name ='hours'></tr></td>"
-    var dateAdded = "";
-    // Capture Button Click
-    $("#submit").on("click", function(event) {
-      event.preventDefault();
+// Initial variables
+//this is the train name
+var train = "";
+//this is where the train is going 
+var destination = "";
+// this is how often the train runs
+var frequency = 0;
+//this is when the train arrives
+var arrival = "";
+//this is the time the first train leaves
+var firstTrain = "";
+// this is how many minutes away the train is
+var minutesAway = 0;
+// the time the next train arrives
+var nextArrival = 0;
+var firsttrainConverted = "";
+// var dateAdded = "";
+// var minutesAway = "";
+// var currentTime= "";
+// Capture Button Click
+$("#submit").on("click", function (event) {
+    event.preventDefault();
 
-      // YOUR TASK!!!
-      // Code in the logic for storing and retrieving the most recent user.
-      // Don't forget to provide initial data to your Firebase database.
-      train = $("#trainName-input").val().trim();
-      destination = $("#destination-input").val().trim();
-       firstTrain= $("#firstTrain-input").val().trim();
-       frequency = $("#frequency-input").val().trim();
-      console.log("this is the train "+train);
+    // YOUR TASK!!!
+    // Code in the logic for storing and retrieving the most recent user.
+    // Don't forget to provide initial data to your Firebase database.
+    train = $("#trainName-input").val().trim();
+    destination = $("#destination-input").val().trim();
+    firstTrain = $("#firstTrain-input").val().trim();
+    frequency = $("#frequency-input").val().trim();
+    currentTime = moment();
+    console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
 
 
-      // Code for the push
-      dataRef.ref().push({
+
+    // Code for the push
+    dataRef.ref().push({
         train: train,
         destination: destination,
         frequency: frequency,
-        arrival: arrival,
-        dateAdded: firebase.database.ServerValue.TIMESTAMP
+        firstTrain: firstTrain,
+        arrival: arrival
+        //dateAdded: firebase.database.ServerValue.TIMESTAMP
 
-      });
     });
-    dataRef.ref().on("child_added", function(childSnapshot) {
-        // Log everything that's coming out of snapshot
-        console.log(childSnapshot.val().train);
-        console.log(childSnapshot.val().destination);
-        console.log(childSnapshot.val().frequency);
-        console.log(childSnapshot.val().arrival);
-        console.log(dateAdded);
-        
-    
-    $("tbody").append(`<tr><td>${childSnapshot.val().train}<td>
-    ${childSnapshot.val().destination}</td><td>
-    ${childSnapshot.val().frequency}</td>`)
 
-}, function(errorObject) {
+});
+dataRef.ref().on("child_added", function (childSnapshot) {
+    // Log everything that's coming out of snapshot
+    console.log("This is the name of the first train" + " " + childSnapshot.val().train);
+    console.log("This is where the train is going" + " " + childSnapshot.val().destination);
+    console.log("this is how often the train runs" + " " + childSnapshot.val().frequency);
+    console.log(childSnapshot.val().arrival);
+    console.log("this is the time the first train leaves" + " " + childSnapshot.val().firstTrain);
+    
+    var firsttrainConverted = moment(firstTrain, "HH:mm").subtract(1, "years");
+
+    // var diffTime = 0;
+    var timeRemainder = 0;
+    var nextArrival = 0;
+    // var dbfrequency = childSnapshot.val().frequency;
+//    var firstleaveTime = childSnapshot.val().firstTrain;
+    //difference between now and the time the first train leaves
+    console.log("CONVERTED" + " " + firsttrainConverted);
+   var diffTime = moment().diff(moment(firsttrainConverted), "minutes");
+    console.log("DIFFERENCE IN TIME: " + diffTime);
+
+console.log("THIS IS THE FREQUENCY" + " " + frequency);
+
+    timeRemainder = diffTime % frequency;
+    //   console.log ("this is the remainder" + " " + timeRemainder);
+// var tMinutesTillTrain = frequency - timeRemainder;
+//     console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
+
+//     nextArrival = moment().add(tMinutesTillTrain, "minutes");
+//     console.log("ARRIVAL TIME: " + moment(nextArrival).format("hh:mm a"));
+
+
+
+    
+    // // Test for correct times and info
+    // console.log(moment().format("X"));
+
+
+    // $("tbody").append(`<tr><td>${childSnapshot.val().train}<td>
+    // ${childSnapshot.val().destination}</td><td>
+    // ${childSnapshot.val().frequency}</td><td>
+    // ${nextTrain}</td>`)
+
+}, function (errorObject) {
     console.log("Errors handled: " + errorObject.code);
-  });
+});
